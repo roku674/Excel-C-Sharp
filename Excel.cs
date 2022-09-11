71762945d1c7ef8bb3afe404dcc42c43c47915ed
roku674 <af03848@georgiasouthern.edu>
2022-09-11 16:35:36 -0400

    Update Excel.cs

--- Excel.cs
+++ Excel.cs
@@ -58,6 +58,42 @@ namespace ExcelCSharp
         public int colCount { get; set; }
         public int rowCount { get; set; }
 
+        public static void ConvertFromCSVtoXLSX(string csv, string xlsx)
+        {
+            //System.Data.DataTable dataTable = ConvertCsvToDataTable(csv); //save datatable to xlsx
+            string copy = System.IO.Path.GetTempFileName();
+            if (System.IO.File.Exists(copy))
+            {
+                System.IO.File.Delete(copy);
+            }
+
+            Application app = new Application();
+            Workbook wb = app.Workbooks.Open(csv, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing);
+            app.DisplayAlerts = false;
+            wb.SaveAs(copy,
+                    XlFileFormat.xlWorkbookDefault,
+                    System.Reflection.Missing.Value,
+                    System.Reflection.Missing.Value,
+                    false,
+                    false,
+                    Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
+                    false,
+                    false,
+                  System.Reflection.Missing.Value,
+                  System.Reflection.Missing.Value,
+                  System.Reflection.Missing.Value);
+            wb.Close();
+            app.Quit();
+
+            if (System.IO.File.Exists(xlsx))
+            {
+                System.IO.File.Delete(xlsx);
+            }
+
+            System.IO.File.Copy(copy, xlsx);
+            System.IO.File.Delete(copy);
+        }
+
         public static async System.Threading.Tasks.Task ConvertFromCSVtoXLSXAsync(string csv, string xlsx)
         {
             //System.Data.DataTable dataTable = ConvertCsvToDataTable(csv); //save datatable to xlsx
@@ -96,32 +132,19 @@ namespace ExcelCSharp
             await System.Threading.Tasks.Task.Delay(5);
         }
 
+        /// <summary>
+        /// Reads each cell by seperating them by comma (if you have commas in the cells this could be prone to failure)
+        /// </summary>
+        /// <param name="csv"></param>
+        /// <returns>2d string array</returns>
+        public static string[][] ReadCSV(string csv)
+        {
+            return System.IO.File.ReadLines(csv).Select(x => x.Split(',')).ToArray();
+        }
+
         public static Dictionary<string, Microsoft.Office.Interop.Excel.Worksheet> GetDictionairy()
         {
             return dict;
-        }
-
-        internal System.Data.DataTable ConvertCsvToDataTable(string filePath)
-        {
-            System.IO.StreamReader sr = new System.IO.StreamReader(filePath);
-            string[] headers = sr.ReadLine().Split(',');
-            System.Data.DataTable dataTable = new System.Data.DataTable();
-            foreach (string header in headers)
-            {
-                dataTable.Columns.Add(header);
-            }
-            while (!sr.EndOfStream)
-            {
-                string[] rows = System.Text.RegularExpressions.Regex.Split(sr.ReadLine(), ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
-                System.Data.DataRow dr = dataTable.NewRow();
-                for (int i = 0; i < headers.Length; i++)
-                {
-                    dr[i] = rows[i];
-                }
-                dataTable.Rows.Add(dr);
-            }
-
-            return dataTable;
         }
 
         /// <summary>
@@ -268,11 +291,6 @@ namespace ExcelCSharp
             return cell;
         }
 
-        public string[][] ReadLines()
-        {
-            return System.IO.File.ReadLines(path).Select(x => x.Split(',')).ToArray();
-        }
-
         /// <summary>
         /// returns the entire excel sheet
         /// </summary>
@@ -287,21 +305,21 @@ namespace ExcelCSharp
         }
 
         /// <summary>
-        /// returns the entire excel sheet
-        /// </summary>
-        /// <returns>2d string arr</returns>
-        public string[,] ReadCellRangeStr()
-        {
-            return ws.UsedRange.Text;
-        }
-
-        /// <summary>
         /// Read the entire excel sheet faster but wont give datetimes
         /// </summary>
         /// <returns>2d object arr</returns>
         public object[,] ReadCellRangeFast()
         {
             return ws.UsedRange.Value2;
+        }
+
+        /// <summary>
+        /// returns the entire excel sheet
+        /// </summary>
+        /// <returns>2d string arr</returns>
+        public string[,] ReadCellRangeStr()
+        {
+            return ws.UsedRange.Text;
         }
 
         /// <summary>
@@ -320,6 +338,11 @@ namespace ExcelCSharp
             {
                 return null;
             }
+        }
+
+        public string[][] ReadLines()
+        {
+            return System.IO.File.ReadLines(path).Select(x => x.Split(',')).ToArray();
         }
 
         public void Save()
@@ -341,6 +364,29 @@ namespace ExcelCSharp
         public void WriteToCell(int row, int col, string s)
         {
             (ws.Cells[row, col] as Microsoft.Office.Interop.Excel.Range).Value = s;
+        }
+
+        internal System.Data.DataTable ConvertCsvToDataTable(string filePath)
+        {
+            System.IO.StreamReader sr = new System.IO.StreamReader(filePath);
+            string[] headers = sr.ReadLine().Split(',');
+            System.Data.DataTable dataTable = new System.Data.DataTable();
+            foreach (string header in headers)
+            {
+                dataTable.Columns.Add(header);
+            }
+            while (!sr.EndOfStream)
+            {
+                string[] rows = System.Text.RegularExpressions.Regex.Split(sr.ReadLine(), ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                System.Data.DataRow dr = dataTable.NewRow();
+                for (int i = 0; i < headers.Length; i++)
+                {
+                    dr[i] = rows[i];
+                }
+                dataTable.Rows.Add(dr);
+            }
+
+            return dataTable;
         }
 
         //public static int ReleaseComObject(object o);
